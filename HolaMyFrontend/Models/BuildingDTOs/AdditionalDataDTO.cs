af544a978920fe69ecd8c86751d23a75b1148cf4
mthutt2k3 <mthutt2k3@gmai.com>
2025-07-17 00:20:46 +0700

    change api for amenity and roomtype

---
+++ HolaMyFrontend/Models/BuildingDTOs/AdditionalDataDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMy.Core.DTOs.BuildingDTOs
+{
+    public class AmenityBuildingDTO
+    {
+        public int AmenityId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string RoomCount { get; set; } = null!;
+    }
+    public class RoomTypeListBuildingDTO
+    {
+        public int RoomTypeId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string RoomCount { get; set; } = null!;
+    }
+}
